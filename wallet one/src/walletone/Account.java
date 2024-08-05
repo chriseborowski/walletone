c00fd6d1a1f0bda92f5da9c037bebae718218b20
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-05 14:27:57 -0500

    Create first file version

---
+++ wallet one/src/walletone/Account.java
@@ -0,0 +1,36 @@
+package walletone;
+
+public abstract class Account {
+
+    static int accountIndex = 100100;
+    String accountNumber;
+    String randomBinNumber = BinList.getRandomBinNumber();
+
+    // List of shared properties for virtual cards
+    public String fullName;
+    public String zipCode;
+
+    // Constructor to set base properties and initialize the card
+    public Account(String fullName, String zipCode) {
+        this.fullName = fullName;
+        this.zipCode = zipCode;
+
+        // Set account number
+        ++accountIndex;
+        this.accountNumber = setAccountNumber();
+    }
+
+    // Create card number
+    private String setAccountNumber() {
+        String firstThreeOfZipCode = zipCode.substring(0, 3);
+        return accountIndex + firstThreeOfZipCode;
+    }
+
+    // List common methods
+    public void showCardInfo() {
+        System.out.println("Full name: " + fullName);
+        System.out.println("ZIP code: " + zipCode);
+        System.out.println("Account number: " + this.accountNumber);
+    }
+
+}
