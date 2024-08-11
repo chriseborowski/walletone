37d942ca5fa0e32388f3de99e31b271193f4c1cc
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-11 08:55:55 -0500

    Add method to return cards by their number

--- wallet one/src/walletone/CardManager.java
+++ wallet one/src/walletone/CardManager.java
@@ -5,16 +5,13 @@ import java.util.List;
 
 public class CardManager {
     private final List<Card> cards;
-    private String fullName;
-    private String zipCode;
 
     public CardManager() {
         this.cards = new ArrayList<>();
     }
 
-    public void addNewCard() {
+    public void addNewCard(String fullName, String zipCode) {
         Card newCard = new Card(fullName, zipCode);
-        newCard.generateCardNumber();
         cards.add(newCard);
         System.out.println("Success! New credit card added.");
     }
@@ -24,7 +21,7 @@ public class CardManager {
             System.out.println("No credit cards to display.");
         }
         else {
-            System.out.println("List of current credit cards: ");
+            System.out.println("List of current credit cards: \n");
 
             for (Card card : cards) {
                 card.showCardInfo();
@@ -32,4 +29,14 @@ public class CardManager {
             }
         }
     }
+
+    public Card findCardByNumber(String cardNumber) {
+        for (Card card : cards) {
+            if (card.getCardNumber().equals(cardNumber)) {
+                return card;
+            }
+        }
+        return null;
+    }
+
 }
