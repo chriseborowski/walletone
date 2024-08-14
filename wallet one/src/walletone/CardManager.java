f2ba779483c776a722a4c166037dc0f2359dcbe9
Chris Borowski <krzysztofeborowski@gmail.com>
2024-08-13 23:12:04 -0500

    Add support for future HashMap functionalities

--- wallet one/src/walletone/CardManager.java
+++ wallet one/src/walletone/CardManager.java
@@ -1,13 +1,16 @@
 package walletone;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class CardManager {
     private final List<Card> cards;
+    private final HashMap<String, Card> cardHashMap;
 
     public CardManager() {
         this.cards = new ArrayList<>();
+        this.cardHashMap = new HashMap<>();
     }
 
     public Card addNewCard(String fullName, String zipCode) {
@@ -15,6 +18,7 @@ public class CardManager {
         newCard.generateCardNumber();
         newCard.addCardUser(fullName, zipCode);
         cards.add(newCard);
+        cardHashMap.put(newCard.getCardNumber(), newCard);
         System.out.println("Success! New credit card added.");
         return newCard;
     }
